b17c776c97ea7d049c4b344f37576dded1b5cf69
Kristian Hadzhikolev <balccbf@gmail.com>
2022-03-28 14:16:38 +0200

    Update BasicSpringApiApplicationTests.java

--- src/test/java/com/Energy/BasicSpringAPI/BasicSpringApiApplicationTests.java
+++ src/test/java/com/Energy/BasicSpringAPI/BasicSpringApiApplicationTests.java
@@ -1,9 +1,6 @@
 package com.Energy.BasicSpringAPI;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
-import org.junit.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
