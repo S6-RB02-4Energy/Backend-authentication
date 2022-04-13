16da4de638421ba6f1684980c8f319399a92fda1
Kristian Hadzhikolev <balccbf@gmail.com>
2022-04-13 13:35:42 +0200

    small changes

--- src/main/java/com/Energy/BasicSpringAPI/WebSecurityConfig.java
+++ src/main/java/com/Energy/BasicSpringAPI/WebSecurityConfig.java
@@ -29,6 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable();
+        http.headers().frameOptions().disable();
+
     }
 
     @Bean
